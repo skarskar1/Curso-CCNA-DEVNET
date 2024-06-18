bb94b543cae762fdefac7175a047a145fff3bbb7
skarskar1 <sc.basualto@duocuc.cl>
2024-06-18 20:51:08 +0000

    Committing Curso CCNA DEVNET

---
+++ ET.py
@@ -0,0 +1,7 @@
+
+firstName = input("Cual es tu nombre? ")
+lastName= input("Cual es tu apellido? ")
+location = input("Cual es tu localización? ")
+age =input("Cual es tu edad? ")
+print("Hola " + firstName, lastName + "! Tu ubicación es "+ location +" y tienes "+
+age +" años.")
